993fc13e9c0d980a9f1da5035ae9dd510ea1505c
VincentReddi <Vincent.Reddi@student.htw-berlin.de>
2025-05-09 12:30:26 +0200

    Meilenstein 2

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import MovieList from './components/MovieList.vue'
+</script>
+
+<template>
+  <div>
+    <h1>PopcornPilot</h1>
+    <MovieList />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
