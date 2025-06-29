c8b9316727a92bd616dbb164815ed4add780a988
VincentReddi <Vincent.Reddi@student.htw-berlin.de>
2025-06-29 21:32:23 +0200

    Laptop Push

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,26 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="home">
+    <h1>🎬 Willkommen bei PopcornPilot</h1>
+    <SearchMovies />
+  </div>
 </template>
 
-<style scoped>
+<script>
+import SearchMovies from '../components/SearchMovies.vue'
 
+export default {
+  components: {
+    SearchMovies
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+h1 {
+  text-align: center;
+  margin-bottom: 30px;
+}
 </style>
